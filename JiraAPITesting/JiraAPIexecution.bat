5c5b7a1abe5d01c19fb3947f63263b5c181693fa
Vijibalu25 <140432231+Vijibalu25@users.noreply.github.com>
2023-11-19 19:47:38 +0000

    Delete JiraAPITesting directory

--- JiraAPITesting/JiraAPIexecution.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Dell\Desktop\TestLeaf Learning\Postman-API(Nov 2023)\Week2Day1\Newman Execution for Jiratest
-
-newman run "Week2-JiraAssignment(Chaning).postman_collection.json" -g workspace.postman_globals.json -e JiraDemo.postman_environment.json -r htmlextra
